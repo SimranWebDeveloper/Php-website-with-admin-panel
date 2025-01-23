83806b13f3fcc436c7f0f1e1f8982c7261e5ce21
SimranWebDeveloper <112202814+SimranWebDeveloper@users.noreply.github.com>
2025-01-23 21:16:15 +0400

    authendication login

--- php-admin-panel/admin/authentication.php
+++ php-admin-panel/admin/authentication.php
@@ -7,24 +7,44 @@ if (isset($_SESSION['auth'])) {
         $role=validate($_SESSION['loggedInUserRole']);
         $email=validate($_SESSION['loggedInUser']['email']);
 
-        $query="SELECT * FORM users WHERE email='$email' AND role='$role' LIMIT 1 ";
+        $query="SELECT * FROM users WHERE email='$email' AND role='$role' LIMIT 1";
         $result=mysqli_query($conn,$query);
-
+        // eger bu adda istifadeci varsa($result gelen array)
         if ($result) {
-            
+            //eger bos bir obyekdirse yeniki hec bir netice gelmeyibse
             if (mysqli_num_rows($result)==0) {
 
                 logoutSession();
                 redirect('../login.php','Access Denied');
             }
-        }else{
+            // eger heminki isdifadecinin email ve pass heqiqeten duzdur ve uygun netice donubse
+            else{
+                $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+                // eger rolu admin deyilse - yeniki user-ler admin panele gire bilmeyecek
+                if ($row['role']!='admin') {
+                    logoutSession();
+                    redirect('../login.php','Access Denied');
+                }
+
+                //eger  ban(1) edilibse
+                if ($row['is_ban']==1) {
+                    logoutSession();
+                    redirect('../login.php','Your account has been banned. Please contact  admin');
+                }
+            }
+        }
+        // eger bu adda istifadeci yoxdursa
+        else{
 
             logoutSession();
             redirect('../login.php','Something Went Wrong!');
 
         }
     }
-}else{
+}
+// eger isdifadeci helem login olmayibsa
+else{
     redirect('../login.php','Login to Continiue...');
 }
 
--- php-admin-panel/admin/logout.php
+++ php-admin-panel/admin/logout.php
@@ -1,6 +1,7 @@
 <?php 
 require '../config/function.php';
 
+//istifadeci login olanda $_SESSION['auth'] deyerini 'true' edirik
 if (isset($_SESSION['auth'])) {
     logoutSession();
     redirect('../login.php','Logged Out Successfully!');
