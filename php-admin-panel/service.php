19af1683645faefa21c0ca29d334016ff2e9e3fb
SimranWebDeveloper <112202814+SimranWebDeveloper@users.noreply.github.com>
2025-02-16 18:31:43 +0400

    ders 14 son

---
+++ php-admin-panel/service.php
@@ -0,0 +1,69 @@
+
+<?php 
+$pageTitle="Services";
+include('includes/header.php');
+
+if (isset($_GET['slug'])) {
+    if ($_GET['slug']==null) {
+        redirect('services.php','');
+    }
+}else{
+    redirect('services.php','');
+
+}
+
+$slug=validate($_GET['slug']);
+$service="SELECT * FROM services WHERE status='0' AND slug='$slug' LIMIT 1 ";
+$result=mysqli_query($conn,$service); 
+
+if (!$result) {
+    redirect('services.php','');
+}
+
+//yuxaridaki url-ni slugdan goturur ve bizde slugi deyisende xeta vermemesi ucun evvelki sehiveye qaytaririq
+if (mysqli_num_rows($result)==0) {
+    redirect('services.php','');
+}
+
+$rowData=mysqli_fetch_assoc($result);
+?>
+
+
+<div class="py-5 bg-secondary">
+    <div class="container">
+        <h4 class="text-white text-center"> <?= $rowData['name'] ?></h4>
+    </div>
+</div>
+
+<div class="py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card card-body shadow-sm">
+                <h4><?= $rowData['name'] ?></h4>
+                <div class="underline"></div>
+                <p>
+                    <?= $rowData['small_description'] ?>
+                </p>
+                <div class="mb-3">
+
+                    <?php if($rowData['image'] != '') : ?>
+                    <img src="admin/<?= $rowData['image']; ?>" class="w-100 rounded" alt="Img" 
+                    />
+                    <?php else: ?>
+                        <img src="assets/images/no-img.jpg"  class="w-100 rounded"  alt="Img" 
+                        />
+                    <?php endif; ?>
+                </div>
+                <p><?= $rowData['long_description'] ?></p>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php include('includes/footer.php')
+?>
--- php-admin-panel/services.php
+++ php-admin-panel/services.php
@@ -4,6 +4,63 @@ $pageTitle="Services";
 include('includes/header.php')
 ?>
 
+<div class="py-5 bg-secondary">
+    <div class="container">
+        <h4 class="text-white text-center">Our Services</h4>
+    </div>
+</div>
+
+<div class="py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <?php
+                $serviceQuery="SELECT * FROM services WHERE status='0' ";
+                $result=mysqli_query($conn,$serviceQuery);
+
+                if ($result) {
+                    if (mysqli_num_rows($result)>0) {
+                        
+                        foreach ($result as $row) {
+                            ?>
+                                <div class="col-md-3 mb-3">
+                                    <div class="card shadow-sm">
+                                        <?php if ($row['image'] !=''):?>
+                                            <img src="admin/<?= $row['image'] ?>" class="w-100 rounded" alt="Img" style="min-height: 200px;max-height:200px;" />
+                                            <?php else :?>
+                                            <img src="assets/images/no-img.jpg" class="w-100 rounded" alt="Img" style="min-height: 200px;max-height:200px;" />
+
+                                        <?php endif;?>
+                                        <div class="card-body">
+                                            <h5><?=$row['name'] ?></h5>
+                                            <p><?=$row['small_description'] ?></p>
+                                            <div>
+                                                <a href="service.php?slug=<?=$row['slug']  ?>" class="text-primary">read more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php
+                        }
+                    }else{
+                        ?>
+                        <div class="col-md-3 mb-3">
+                            <h5>No Service Avilable! </h5>
+                        </div>
+                        <?php
+                    }
+                }else{
+                    ?>
+                    <div class="col-md-3 mb-3">
+                        <h5>Something Went Wrong! </h5>
+                    </div>
+                    <?php
+                }
+            ?>
+
+
+        </div>
+    </div>
+</div>
 
 
 
