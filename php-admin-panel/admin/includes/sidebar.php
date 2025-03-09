9eb4e09bf1657959eb2cae8b7a2cfd2974a08e67
SimranWebDeveloper <112202814+SimranWebDeveloper@users.noreply.github.com>
2025-03-09 13:04:31 +0400

    part-23 activ link yazmaq

--- php-admin-panel/admin/includes/sidebar.php
+++ php-admin-panel/admin/includes/sidebar.php
@@ -1,3 +1,17 @@
+  
+<!-- $_SERVER['SCRIPT_NAME'];  -butun sehiveni url-sini verir -->
+
+<!-- strrpos($_SERVER['SCRIPT_NAME'], "/")
+    Bu fonksiyon, dosya yolundaki son / karakterinin konumunu bulur. Böylece dosya adının nerede başladığını belirler. -->
+
+<!-- substr($_SERVER['SCRIPT_NAME'], strrpos($_SERVER['SCRIPT_NAME'], "/") + 1)
+substr fonksiyonu, bulduğu konumun hemen sonrasından itibaren kalan metni (yani dosya adını) alır.
+Bura /-dan basladigina gore +1 yaziriq ki /-dan sonrasini alsin
+-->
+<?php
+$pageName= substr($_SERVER['SCRIPT_NAME'], strrpos($_SERVER['SCRIPT_NAME'],"/") +1);
+?>
+
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
@@ -12,9 +26,9 @@
       <ul class="navbar-nav">
 
         <li class="nav-item">
-          <a class="nav-link  active" href="index.php">
+          <a class="nav-link  <?= $pageName=='index.php' ? 'active':''; ?>" href="index.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="fa fa-home text-white text-lg"></i>
+                <i class="fa fa-home   text-lg  <?= $pageName=='index.php' ? 'text-white':'text-dark'; ?> "></i>
                 
             </div>
             <span class="nav-link-text ms-1">Dashboard</span>
@@ -26,9 +40,9 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link  " href="enquiries.php">
+          <a class="nav-link  <?= $pageName=='enquiries.php' ? 'active':''; ?>" href="enquiries.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white  text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="fa fa-bullhorn text-dark text-lg"></i>
+                <i class="fa fa-bullhorn  text-lg <?= $pageName=='enquiries.php' ? 'text-white':'text-dark'; ?>"></i>
             </div>
             <span class="nav-link-text ms-1">Enquiries</span>
           </a>
@@ -40,9 +54,15 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link  " href="services.php">
+          <a class="nav-link  
+          <?= $pageName=='services.php' ? 'active':''; ?>
+          <?= $pageName=='services-create.php' ? 'active':''; ?>
+          <?= $pageName=='services-edit.php' ? 'active':''; ?>
+          
+          " 
+          href="services.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white  text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="fa fa-cogs text-dark text-lg"></i>
+                <i class="fa fa-cogs text-lg <?= $pageName=='services.php' ? 'text-white':'text-dark'; ?> "></i>
             </div>
             <span class="nav-link-text ms-1">Services</span>
           </a>
@@ -54,27 +74,36 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link  " href="users.php">
+          <a class="nav-link  
+          <?= $pageName=='users.php' ? 'active':''; ?>
+          <?= $pageName=='users-create.php' ? 'active':''; ?>
+          <?= $pageName=='users-edit.php' ? 'active':''; ?>
+          
+          " href="users.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white  text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="fa fa-user-plus text-dark text-lg"></i>
+                <i class="fa fa-user-plus text-lg <?= $pageName=='users.php' ? 'text-white':'text-dark'; ?>"></i>
             </div>
             <span class="nav-link-text ms-1">Admin / Users</span>
           </a>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link  " href="social-media.php">
+          <a class="nav-link  
+          <?= $pageName=='social-media.php' ? 'active':''; ?>
+          <?= $pageName=='social-media-create.php' ? 'active':''; ?>
+          <?= $pageName=='social-media-edit.php' ? 'active':''; ?>
+          " href="social-media.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white  text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="fa fa-globe text-dark text-lg"></i>
+                <i class="fa fa-globe text-lg <?= $pageName=='social-media.php' ? 'text-white':'text-dark'; ?>"></i>
             </div>
             <span class="nav-link-text ms-1">Social Media</span>
           </a>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link  " href="settings.php">
+          <a class="nav-link  <?= $pageName=='settings.php' ? 'active':''; ?>" href="settings.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white  text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="fa fa-globe text-dark text-lg"></i>
+                <i class="fa fa-globe text-lg <?= $pageName=='settings.php' ? 'text-white':'text-dark'; ?>"></i>
             </div>
             <span class="nav-link-text ms-1">Settings</span>
           </a>
